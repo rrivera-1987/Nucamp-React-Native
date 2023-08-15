3a0780d7f917192c9a7b3c23f4432dea59a38f37
Ramon A. Rivera <ramonrivera550@gmail.com>
2023-08-15 09:05:16 -0400

    Navigation Icons

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,8 +1,22 @@
-import { ScrollView } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 import { Card, ListItem, Avatar } from 'react-native-elements';
 import { useState } from 'react';
-import Mission from './Mission';
 import { PARTNERS } from '../shared/partners';
+
+const Mission = () => {
+    return (
+        <Card>
+            <Card.Title>Our Mission</Card.Title>
+            <Card.Divider />
+            <Text style={{ margin: 10 }}>
+                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness.
+                We increase access to adventure for the public while promoting safe and respectful use of resources.
+                The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards.
+                We also present a platform for campers to share reviews on campsites they have visited with each other.
+            </Text>
+        </Card>
+    );
+};
 
 const AboutScreen = () => {
     const [partners] = useState(PARTNERS);
