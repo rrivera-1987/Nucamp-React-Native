651bb3beaf8e6d54ba0f46ad187d06e3143ee02f
Ramon A. Rivera <ramonrivera550@gmail.com>
2023-08-16 22:49:08 -0400

    Activity Indicator

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,6 +2,7 @@ import { ScrollView, Text } from 'react-native';
 import { Avatar, Card, ListItem } from 'react-native-elements';
 import { useSelector } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from '../components/LoadingComponent';
 
 function Mission() {
     return (
@@ -9,14 +10,10 @@ function Mission() {
             <Card.Title>Our Mission</Card.Title>
             <Card.Divider />
             <Text style={{ margin: 10 }}>
-                We present a curated database of the best campsites in the vast
-                woods and backcountry of the World Wide Web Wilderness. We
-                increase access to adventure for the public while promoting safe
-                and respectful use of resources. The expert wilderness trekkers
-                on our staff personally verify each campsite to make sure that
-                they are up to our standards. We also present a platform for
-                campers to share reviews on campsites they have visited with
-                each other.
+                We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness.
+                We increase access to adventure for the public while promoting safe and respectful use of resources.
+                The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards.
+                We also present a platform for campers to share reviews on campsites they have visited with each other.
             </Text>
         </Card>
     );
@@ -25,6 +22,31 @@ function Mission() {
 const AboutScreen = () => {
     const partners = useSelector((state) => state.partners);
 
+    if (partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Loading />
+                </Card>
+            </ScrollView>
+        );
+    }
+
+    if (partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider />
+                    <Text>{partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
     return (
         <ScrollView>
             <Mission />
